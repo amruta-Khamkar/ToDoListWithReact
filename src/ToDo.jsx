7cc786d61abedaa6cc720d98f37c0b8892103d07
amruta-khamkar <amrutakhamkar15@gmail.com>
2021-09-27 02:34:21 +0530

    adding number of tasks

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -7,100 +7,112 @@ class ToDo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            arr: [
+            tasks: [
                 // { id: 1, Title: 'JS Batch', body: "React js" },
-                // { id: 2, Title: 'Philomaths', body: "My Team" },
-            ], 
-            edit:0
-
+                // { id: 2, Title: 'My company', body: "Neosoft Techonlogies" },
+            ],
+            update: 0,
+            noOfTasks:1
         }
     }
 
-    handler1 = (event) => {
-        let {name,value}=event.target;
-        this.setState({[name]:value})
+    handler = (event) => {
+        let { name, value } = event.target;
+        this.setState({ [name]: value });
     };
-    
-    
-    addTask = () => {
-        let { arr, Title, body ,id} = this.state;
-        if(document.getElementById("title").value=='' || document.getElementById("desc").value==''){
+
+   
+    add = () => {
+        let { tasks, Title, body, noOfTasks} = this.state;
+        if (document.getElementById("title").value == '' || document.getElementById("desc").value == ' ') {
             alert("Please fill out fields");
         }
-        else{
-            this.setState({arr:[...arr,{Title:Title,body:body,id:id+1}]});
-            document.getElementById("title").value=" ";
-            document.getElementById("desc").value=" "
+        else {
+            this.setState({ tasks: [...tasks, { Title: Title, body: body, }] });
+            this.setState({noOfTasks:this.state.tasks.length})
+            console.log(this.state.tasks.length)
+            document.getElementById("title").value = " ";
+            document.getElementById("desc").value = " ";
+            console.log(this.state)
         }
     }
 
 
-    deleteTask = (index) => {
+    delete = (index) => {
         console.log("delete");
+        let noOfTasks=this.state.noOfTasks;
         var bool = window.confirm("Do You really want to delele this?")
         if (bool == true) {
-            let { arr } = this.state;
-            arr.splice(index, 1)
+            let { tasks } = this.state;
+            tasks.splice(index, 1)
             this.setState({
-                arr: arr
+                tasks: tasks
             })
+            this.setState({noOfTasks:this.state.noOfTasks-1})
+            console.log(noOfTasks)
         }
     }
-
-    updateTask = (index) => {
-        let { arr ,Title,body,edit} = this.state;
-       this.setState({edit:index})
-       console.log(this.state)
-       document.getElementById("title").value=arr[index].Title
-       document.getElementById("desc").value=arr[index].body
+  
+    edit = (index) => {
+        let { tasks, Title, body, update } = this.state;
+        this.setState({ update: index })
+        console.log(this.state)
+        document.getElementById("title").value = tasks[index].Title
+        document.getElementById("desc").value = tasks[index].body
     }
 
-    finalUpdate=(position)=>{
-        if(window.confirm("Are sure you want to update this?")){
+    Update = (position) => {
+        if (document.getElementById("title").value == '' || document.getElementById("desc").value == ' ') {
+            alert("Please Select the task that you want to update !");
+        }
+
+        else if (window.confirm("Are sure you want to update this?")) {
             console.log("final update")
-            let { arr ,Title,body} = this.state;
-            arr[position].Title= document.getElementById("title").value;
-            arr[position].body= document.getElementById("desc").value;
-            console.log(arr[position].Title)
-            this.setState({Title:Title,body:body})
-            document.getElementById("title").value=" ";
-            document.getElementById("desc").value=" "
+            let { tasks, Title, body } = this.state;
+            tasks[position].Title = document.getElementById("title").value;
+            tasks[position].body = document.getElementById("desc").value;
+            console.log(tasks[position].Title)
+            this.setState({ Title: Title, body: body })
+            document.getElementById("title").value = " ";
+            document.getElementById("desc").value = " "
         }
     }
     render() {
         return <>
             <div className="Container">
                 <h1>To Do List</h1>
-                <label> Title: </label><input onChange={this.handler1} name="Title" id="title" cols="30" rows="3" value={this.state.arr.Title}></input><br></br>
-                <label className="label">Description :</label> <textarea  onChange={this.handler1} name="" id="desc" name="body" cols="30" rows="3" o></textarea><br></br>
+                    <h1 className="nums">No of tasks are :<span className="red">{this.state.noOfTasks+1}</span></h1>
+                    
+                <label> Title: </label><input onChange={this.handler} name="Title" id="title" cols="30" rows="3"></input><br></br>
+                <label className="label">Description :</label> <textarea onChange={this.handler} name="" id="desc" name="body" cols="30" rows="3" o></textarea><br></br>
                 <div className="flex">
-                <button className="btn" onClick={this.addTask}>Add task</button><br></br>
-                <button className="btn" onClick={() => this.finalUpdate(this.state.edit)}>Final Update</button><br></br>
+                    <button className="btn" onClick={this.add}>Add task</button><br></br>
+                    <button className="btn" onClick={() => this.Update(this.state.update)}>Final Update</button><br></br>
                 </div>
                 <div className="scroll">
-                <table className="table" border="3">
-                    <thead>
-                        <th>Sr No</th>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Action</th>
-                    </thead>
+                    <table className="table" border="3">
+                        <thead>
+                            <th>Sr No</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Action</th>
+                        </thead>
 
-                    <tbody>
-                        {this.state.arr.map((task, index) => {
-                            return <tr key={index}>
-                                <td>{index + 1}</td>
-                                <td>{task.Title}</td>
-                                <td>{task.body}</td>
-                                <td><button className="btn1" onClick={() => this.updateTask(index)}>Update</button><button className="btn1" onClick={() => this.deleteTask(index)}>Delete</button></td>
-                            </tr>
+                        <tbody>
+                            {this.state.tasks.map((task, index) => {
+                                return <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{task.Title}</td>
+                                    <td>{task.body}</td>
+                                    <td><button className="btn1" onClick={() => this.edit(index)}>Edit</button><button className="btn2" onClick={() => this.delete(index)}>Delete</button></td>
+                                </tr>
 
-                        })}
-                    </tbody>
+                            })}
+                        </tbody>
 
-                </table>
+                    </table>
                 </div>
-               
+
             </div>
         </>
     }
